a14f8bc3c0bd6b90fba75d15ca830d97f9ce7238
Sachin <sachinkaushikofficial@gmail.com>
2025-03-09 16:27:39 +0530

    Spring boot profiles - For Dockerizing Microservices

--- src/main/java/com/sachinsk/company_microservice/company/clients/ReviewClient.java
+++ src/main/java/com/sachinsk/company_microservice/company/clients/ReviewClient.java
@@ -4,7 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("REVIEW-MICROSERVICE")
+@FeignClient(name = "REVIEW-MICROSERVICE",
+        url = "${review-microservice.url}")
 public interface ReviewClient {
 
     @GetMapping("/reviews/averageRating")
